22f6dd8825560ffe48a58a71496d6efbe9f6c59f
maghwa <elkamilmarwa218@gmail.com>
2022-12-29 14:25:58 +0100

    modify the fxml login

--- src/main/java/sample/Login.java
+++ src/main/java/sample/Login.java
@@ -8,9 +8,10 @@ import javafx.event.*;
 
 import java.io.IOException;
 public class Login {
-    public Login(){
+    public Login() {
 
     }
+
     @FXML
     private Button button;
     @FXML
@@ -20,34 +21,19 @@ public class Login {
     @FXML
     private TextField password;
 
-    public void userLogin(ActionEvent event) throws IOException{
+    public void userLogin(ActionEvent event) throws IOException {
         checkLogin();
     }
 
-    private void checkLogin() throws IOException{
+    private void checkLogin() throws IOException {
         Main m = new Main();
-        if(username.getText().toString().equals("marwa")&& password.getText().toString().equals("2022")){
+        if (username.getText().toString().equals("marwa") && password.getText().toString().equals("2022")) {
             wrongLogin.setText("Sucess!!");
-            m.changeScene("afterLogin.fxml");
-        } else if (username.getText().isEmpty()&& password.getText().isEmpty()) {
+            //m.changeScene("Home.fxml");
+        } else if (username.getText().isEmpty() && password.getText().isEmpty()) {
             wrongLogin.setText("Please enter your data");
-        }
-        else{
+        } else {
             wrongLogin.setText("Wrong username or password!");
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,31 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
-    //we need this variable to change the scenes
-    private static Stage stg;
-
     @Override
-    public void start(Stage PrimaryStage) throws IOException {
-        stg = PrimaryStage;
-        PrimaryStage.setResizable(false);
-        Parent root =FXMLLoader.load(getClass().getResource("Login.fxml"));
-        //FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
-        //Scene scene = new Scene(fxmlLoader.load());
-        PrimaryStage.setScene(new Scene(root));
-        PrimaryStage.setTitle("ManaGenie!!");
-        PrimaryStage.show();
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setResizable(true);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Login.fxml"));
+        fxmlLoader.setRoot(new AnchorPane());
+        Parent root = fxmlLoader.load();
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("ManaGenie!!");
+        primaryStage.show();
     }
-    public void changeScene(String fxml) throws IOException{
-       Parent pane= FXMLLoader.load(getClass().getResource(fxml));
-        stg.getScene().setRoot(pane);
-    }
-
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
